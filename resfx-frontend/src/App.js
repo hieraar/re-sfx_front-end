7de3a735051c506ca733dc44d77da222d7da35ee
hieraar <hiera.ahm2002@mail.ugm.ac.id>
2024-02-02 23:54:56 +0700

    menambahkan sign up

--- resfx-frontend/src/App.js
+++ resfx-frontend/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import SignIn from './component/SignIn';
+import SignUp from './component/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
